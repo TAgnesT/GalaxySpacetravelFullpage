79a004e6467e078c33ec0f30fe877039b6f96c06
TAgnesT <gmagiic@hotmail.com>
2023-02-02 15:20:35 +0100

    footer fix ok

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,27 +10,27 @@ const Footer = () => {
                 <div className="location">
                     <FaSearchLocation size={20} style={{color: '#fff', marginRight: '2rem'}} />
                     <div>
-                        <p>Űr utca 1.</p>
-                        <h4>1094 Budapest</h4>
+                        <p>Space Street 1.</p>
+                        <h4>24570 Mars</h4>
                     </div>
                 </div>
 
                 <div className="phone">
                     <h4>
                         <FaPhone size={20} style={{color: '#fff', marginRight: '2rem'}} />
-                        06-80-123-4567
+                        +30-80-123-4567
                     </h4>
                 </div>
                 <div className="email">
                     <h4>
                         <FaMailBulk size={20} style={{color: '#fff', marginRight: '2rem'}} />
-                        utazas@galaxytravel.hu
+                        spacetravelfirst@galaxytravel.com
                     </h4>
                 </div>
             </div>
 
             <div className="right">
-                <h4>Rólunk</h4>
+                <h4>About Us</h4>
                 <p>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, itaque sequi optio rem beatae illum odit praesentium molestias mollitia aperiam. In quae consequuntur autem consequatur, dolor at vero ut aspernatur?"</p>
                 <div className="social">
                     <FaFacebook size={30} style={{color: '#fff', marginRight: '1rem' }} />
